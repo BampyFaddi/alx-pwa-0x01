8d4cedcf770337b6a4eb822d9a3e55eab1fd9fd0
GeoffreyMutisya <101920210+BampyFaddi@users.noreply.github.com>
2025-10-25 17:19:06 +0300

    Fix Button props type issue

--- alx-movie-app/components/layouts/Button.tsx
+++ alx-movie-app/components/layouts/Button.tsx
@@ -9,7 +9,7 @@ const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
     <button
       onClick={action}
-      className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+      className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
     >
       {title}
     </button>
